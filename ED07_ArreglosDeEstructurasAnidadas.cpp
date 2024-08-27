e23ef92c372965cc72e8d12aa2180e2d55727139
Dalí Robles <49328533+DamianRobles@users.noreply.github.com>
2024-08-27 11:45:03 -0600

    declaracion strcuts

---
+++ ED07_ArreglosDeEstructurasAnidadas.cpp
@@ -0,0 +1,36 @@
+/*
+  Fecha: martes 27 agosto de 2024
+  Autor: Dali
+  Practica No.: 7
+  Objetivo: Arreglos de estructuras anidadas
+*/
+
+// librerias
+#include <iostream>
+using namespace std;
+
+// TDA
+struct domicilio{
+    char calle[25];
+    int numero;
+    char colonia[30];
+    int cp;
+};
+struct nombre{
+    char nomPila[30];
+    char aPaterno[30];
+    char aMaterno[30];
+};
+
+// prototipo de funcion
+
+
+int main(void){
+  // declaracion de variables
+
+  return 0;
+} // main
+
+// definicion de funciones
+
+// fin de archivo
