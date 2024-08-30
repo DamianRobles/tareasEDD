fac203cab75412de948e5c4f542079068ad9109c
Dalí Robles <49328533+DamianRobles@users.noreply.github.com>
2024-08-29 21:02:29 -0600

    correccion mayor

--- ED08_ArreglosEstructurasAnidadas_Dali.cpp
+++ ED08_ArreglosEstructurasAnidadas_Dali.cpp
@@ -6,15 +6,10 @@
 */
 
 /*
-*Deberás utilizar la estructura "MiFactura" usada en la Actividad #3  (5 elementos mínimo) práctica ED05_MiFactura_Nombre_del_Alumno.cpp
-*
-*Uno de sus elementos deberá ser un campo tipo de dato de otra estructura creada por medio de struct (estructura anidada). 
-*
 *Deberás  leer y almacenar los datos para un arreglo de 3 elementos tipo de dato tu estructura anidada (arreglo de estructuras anidadas).
-*
 *Implementar una función para leer una variable del tipo de dato de tu propia estructura anidada, la función la devolverá y será asignada a cada elemento de tu arreglo de estructuras declarado en el main( ).
-*
-*Implementar una función que deberá recibir e imprimir todos los elementos de tu arreglo de estructuras. Puede recibir todo el arreglo para imprimirlo o bien, puede recibir cada elemento del arreglo, uno por uno e imprimirlos.
+*Implementar una función que deberá recibir e imprimir todos los elementos de tu arreglo de estructuras. Puede recibir todo el arreglo para imprimirlo o bien, puede recibir cada elemento del arreglo, 
+*uno por uno e imprimirlos.
 */
 
 /*
@@ -24,101 +19,80 @@
 *Estilo del código fuente correcto: sangrías, comentarios, nombres de variables (1 punto)
 */
 
-
 // librerias
 #include <iostream>
 using namespace std;
 
 // TDA
-struct domicilio{
-  char calle[25];
-  int numero;
-  char colonia[30];
-  int cp;
+struct factura{
+  struct sat{
+    char rfc[14];
+    char nombreEmpresa[99];
+    int importeTotal;
+  };
+  char nomCompleto[99];
+  int numFactura;
+  char concepto[99];
+  sat datosFiscales;  
 };
-struct nombre{
-  char nomPila[30];
-  char aPaterno[30];
-  char aMaterno[30];
-};
-struct amigo{
-  nombre nomAmigo;
-  domicilio domAmigo;
-  char numTel[20];
-};
-
-
-// constantes
-const int MAX=3;
 
 // prototipo de funcion
-void imprimir(amigo, int);
-amigo leer(void);
+void imprimirFactura(factura);
 
 int main(void){
   // declaracion de variables
-  amigo amigoNuevo[MAX];
-
-  cout << "Este programa ejemplifica eluso de arreglos de estructuras anidadas" << endl;
-
-  // leer datos
-  for(int i=0; i<MAX; i++){
-      cout << "Ingresa los datos de tu nuevo amigo " << i+1 << endl;
-    amigoNuevo[i] = leer();
-  } //for i
+  factura fact1 = {
+    // datos factura
+    "Beatriz Pinzon Solano", 25, "Prestamo TERRAMODA",
+    // datos fiscales (estructura anidada sat)
+    {"PISB740321EM5", "ECOMODA", 50000000}
+  };
+  factura fact2, fact3;
   
-  // imprimir datos
-  for(int i=0; i<MAX; i++){
-    imprimir(amigoNuevo[i], i+1);
-  } // for i
+  // solicitar datos
+  cout << "Ingrese el nombre de la empresa: ";
+  cin.getline(fact2.datosFiscales.nombreEmpresa, 99);
+  
+  cout << "Ingrese el numero de la factura: ";
+  cin >> fact2.numFactura;
+  
+  cout << "Ingrese el nombre completo del comprador: ";
+  cin.ignore();
+  cin.getline(fact2.nomCompleto, 99);
+  
+  cout << "Ingrese el RFC del comprador: ";
+  cin.getline(fact2.datosFiscales.rfc, 14);
+  
+  cout << "Ingrese el motivo de la factura: ";
+  cin.getline(fact2.concepto, 99);
+  
+  cout << "Ingrese el importe total de la factura: $";
+  cin >> fact2.datosFiscales.importeTotal;
 
+
+  // llamar funcion para imprimir las facturas
+  cout << endl;
+  imprimirFactura(fact1);
+  cout << endl;
+  imprimirFactura(fact2);
+  cout << endl;
+  // se copian los valores de la factura 1 a la factura 3
+  fact3 = fact2;
+  imprimirFactura(fact3);
+  
   return 0;
 } // main
 
-// implementacion de funciones
-//=========================================================
-amigo leer(void){
-  amigo amigoN;
-
-  // leer datos
-  cout << "Nombre de pila (ej. Juan): ";
-  cin.getline(amigoN.nomAmigo.nomPila, 30, '\n');
-  cout << "Apellido paterno: ";
-  cin.getline(amigoN.nomAmigo.aPaterno, 30, '\n');
-  cout << "Apellido materno: ";
-  cin.getline(amigoN.nomAmigo.aMaterno, 30, '\n');
-
-  cout << "Domicilio: ";
-  cin.getline(amigoN.domAmigo.calle, 25, '\n');
-  cout << "Colonia: ";
-  cin.getline(amigoN.domAmigo.colonia, 25, '\n');
-  cout << "Numero: ";
-  cin >> amigoN.domAmigo.numero;
-  cout << "Codigo Postal: ";
-  cin >> amigoN.domAmigo.cp;
-  
-  cin.ignore();
-  cout << "Numero telefonico: ";
-  cin.getline(amigoN.numTel, 20, '\n');
-
-  return amigoN;
-} // leer
-
-//=========================================================
-void imprimir(amigo datos, int num){
-  cout << "====================================================" << endl;
-  cout << "Datos de tu nuevo amigo " << num << endl;
-  cout << "Nombre de pila      : " << datos.nomAmigo.nomPila << endl;
-  cout << "Apellido paterno    : " << datos.nomAmigo.aPaterno << endl;
-  cout << "Apellido materno    : " << datos.nomAmigo.aMaterno << endl;
-  cout << "Domicilio (calle)   : " << datos.domAmigo.calle << endl;
-  cout << "Domicilio (colonia) : " << datos.domAmigo.colonia << endl;
-  cout << "Numero              : " << datos.domAmigo.numero << endl;
-  cout << "Codigo Postal       : " << datos.domAmigo.cp << endl;
-  cout << "Numero telefonico   : " << datos.numTel << endl;
-  cout << "====================================================" << endl;
-  
-  return;
-} // imprimir
+// definicion de funciones
+void imprimirFactura(factura factura){
+  cout << "--------------------- Factura ---------------------" << endl;
+  cout << "Nombre de la empresa: " << factura.datosFiscales.nombreEmpresa << endl;
+  cout << "Numero de la factura: " << factura.numFactura << endl;
+  cout << "Nombre del comprador: " << factura.nomCompleto << endl;
+  cout << "RFC del comprador: " << factura.datosFiscales.rfc << endl;
+  cout << "Motivo de la factura: " << factura.concepto << endl;
+  cout << "Importe total de la factura: $" << factura.datosFiscales.importeTotal << endl;
+  cout << "---------------------------------------------------" << endl;
+} // funcion
 
 // fin de archivo
