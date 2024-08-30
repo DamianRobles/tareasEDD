07b0bbe22113337682f38da8d8bc0d8feed9e782
Dalí Robles <49328533+DamianRobles@users.noreply.github.com>
2024-08-29 21:21:38 -0600

    update

--- ED08_ArreglosEstructurasAnidadas_Dali.cpp
+++ ED08_ArreglosEstructurasAnidadas_Dali.cpp
@@ -36,56 +36,56 @@ struct factura{
   sat datosFiscales;  
 };
 
+//* constantes
+const int MAX=3;
+
 //* prototipo de funcion
-void imprimirFactura(factura);
+factura leerFactura(void);
+void imprimirFactura(factura, int);
 
 int main(void){
   //* declaracion de variables
-  factura fact1 = {
-    //* datos factura
-    "Beatriz Pinzon Solano", 25, "Prestamo TERRAMODA",
-    //* datos fiscales (estructura anidada sat)
-    {"PISB740321EM5", "ECOMODA", 50000000}
-  };
-  factura fact2, fact3;
+  factura factura[MAX];
   
   //* solicitar datos
-  cout << "Ingrese el nombre de la empresa: ";
-  cin.getline(fact2.datosFiscales.nombreEmpresa, 99);
-  
-  cout << "Ingrese el numero de la factura: ";
-  cin >> fact2.numFactura;
-  
-  cout << "Ingrese el nombre completo del comprador: ";
-  cin.ignore();
-  cin.getline(fact2.nomCompleto, 99);
-  
-  cout << "Ingrese el RFC del comprador: ";
-  cin.getline(fact2.datosFiscales.rfc, 14);
-  
-  cout << "Ingrese el motivo de la factura: ";
-  cin.getline(fact2.concepto, 99);
-  
-  cout << "Ingrese el importe total de la factura: $";
-  cin >> fact2.datosFiscales.importeTotal;
-
+  for(int i=0; i<MAX; i++){
+    factura[i] = leerFactura();
+  }
 
   //* llamar funcion para imprimir las facturas
-  cout << endl;
-  imprimirFactura(fact1);
-  cout << endl;
-  imprimirFactura(fact2);
-  cout << endl;
-  //* se copian los valores de la factura 1 a la factura 3
-  fact3 = fact2;
-  imprimirFactura(fact3);
+  for(int i=0; i<MAX; i++){
+    imprimirFactura(factura[i], i+1);
+  }
   
   return 0;
 } //* main
 
 //* definicion de funciones
-void imprimirFactura(factura factura){
-  cout << "--------------------- Factura ---------------------" << endl;
+factura leerFactura(){
+  factura datos;
+  cout << "Ingrese el nombre de la empresa: ";
+  cin.getline(datos.datosFiscales.nombreEmpresa, 99);
+  
+  cout << "Ingrese el numero de la factura: ";
+  cin >> datos.numFactura;
+  
+  cout << "Ingrese el nombre completo del comprador: ";
+  cin.ignore();
+  cin.getline(datos.nomCompleto, 99);
+  
+  cout << "Ingrese el RFC del comprador: ";
+  cin.getline(datos.datosFiscales.rfc, 14);
+  
+  cout << "Ingrese el motivo de la factura: ";
+  cin.getline(datos.concepto, 99);
+  
+  cout << "Ingrese el importe total de la factura: $";
+  cin >> datos.datosFiscales.importeTotal;
+}
+
+//* ==================================================================================
+void imprimirFactura(factura factura, int num){
+  cout << "--------------------- Factura no." << num <<" ---------------------" << endl;
   cout << "Nombre de la empresa: " << factura.datosFiscales.nombreEmpresa << endl;
   cout << "Numero de la factura: " << factura.numFactura << endl;
   cout << "Nombre del comprador: " << factura.nomCompleto << endl;
