7908587acda181974a9d1542f0da98e496339c37
Dalí Robles <49328533+DamianRobles@users.noreply.github.com>
2024-09-03 12:32:10 -0600

    new file

---
+++ ED11_ApuntadoresArreglosNumericos.cpp
@@ -0,0 +1,24 @@
+/*
+  ! Fecha: martes 3 de septiembre de 2024
+  ! Autor: Dali
+  ! Practica No.: 11
+  ! Objetivo: 
+*/
+
+//* librerias
+#include <iostream>
+using namespace std;
+
+//* prototipos
+
+int main(void){
+  // declaracion de variables
+
+  
+  return 0;
+} //* main
+
+//* implementacion de funciones
+
+
+//* fin de archivo
