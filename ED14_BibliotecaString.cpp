73e1622f71f1bd2363a1e8617ac8017a4f53a11c
Dalí Robles <49328533+DamianRobles@users.noreply.github.com>
2024-09-10 11:29:37 -0600

    nuevo archivo

---
+++ ED14_BibliotecaString.cpp
@@ -0,0 +1,25 @@
+/*
+  ! Fecha: Lunes 16 de septiembre de 2024
+  ! Autor: Dali
+  ! Practica No.: 14
+  ! Objetivo: Aritmetica de apuntadores
+*/
+
+//* librerias
+#include <iostream>
+using namespace std;
+
+//* prototipos
+
+const int MAX=5;
+
+//* funcion principal
+int main(void){
+  // declaracion de variables
+
+} //* main
+
+//* implementacion de funciones
+
+
+//* fin de archivo
