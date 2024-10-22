48b1a3c0718ca62c581dca43d5207d5a7b022f07
Dalí Robles <49328533+DamianRobles@users.noreply.github.com>
2024-10-22 12:23:12 -0600

    Update ED26_LDEC.cpp

--- ED26_LDEC.cpp
+++ ED26_LDEC.cpp
@@ -74,13 +74,130 @@ int main(void) {
 //! agregar()
 //! ==============================================================
 void agregar() {
-} // agregar
+  // 1) declarar el apuntador
+  cancion *nueva;
+
+  // 2) solicitar la memoria
+  nueva = (cancion *) malloc(sizeof(cancion));
+
+  // 3) validar el apuntador
+  if(nueva == NULL){
+    cout << "No hay memoria suficiente para registrar una nueva tarea" << endl;
+    return;
+  }
+
+  // 4) Solicitar y almacenar los datos
+  cout <<  endl << endl;
+  cin.ignore();
+  cout << endl << "Ingresa los datos de la nueva cancion" << endl;
+  cout << "============================" << endl;
+  cout << "Titulo: ";
+  cin.getline(nueva->titulo,50,'\n');
+  cout << "Artista: ";
+  cin.getline(nueva->artista,50,'\n');
+  cout << "Duracion (mm:ss)): ";
+  cin.getline(nueva->duracion,6,'\n');
+  cout << "Genero: ";
+  cin.getline(nueva->genero,30,'\n');
+
+  // 5) agregar la tarea al inicio de la LDEA
+  // caso A) lista vacia
+  if(apPlayList == NULL){
+    apPlayList = nueva;
+    apPlayList -> next = apPlayList;
+    apPlayList -> before = apPlayList;
+    cout << "La cancion fue agregada a la playlist" << endl;
+    return;
+  } // if lista vacia
+  
+  // caso B) lista no vacia
+  nueva-> next = apPlayList->next;
+  nueva->before = apPlayList;
+  nueva->next->before = nueva;
+  apPlayList->next = nueva;
+  cout << "La cancion fue agregada a la playlist" << endl;
+  return;
+} // agregar()  
+
+//! consultar()
+//! ==============================================================
+void consultar() {
+  // declarar variable
+  cancion *apCopia = apPlayList;
+  
+  // validar si esta vacia
+  if(apCopia == NULL){
+    cout << "La playlist esta vacia" << endl;
+    return;
+  }
+  
+  // mostrar las canciones
+  cout << endl << "Cancionces guardadas en la playlist" << endl;
+  do{
+    // mostrar  cancion
+    cout << endl << "============================" << endl;
+    cout << "Titulo: " << apCopia->titulo << endl;
+    cout << "Artista: " << apCopia->artista << endl;
+    cout << "Duracion: " << apCopia->duracion << endl;
+    cout << "Genero: " << apCopia->genero << endl;
+    
+    // ir a la cancion siguiente
+    apCopia = apCopia -> next;
+  } while(apPlayList != apCopia);
+  
+  return;
+} // consultar
+
+//! eliminar()
+//! ==============================================================
+void eliminar() {
+} // eliminar
+
+//! buscar()
+//! ==============================================================
+void buscar() {
+  // declarar variable
+  cancion *apCopia = apPlayList;
+  char titBuscar[50];
+  
+  // validar si esta vacia
+  if(apCopia == NULL){
+    cout << "La playlist esta vacia" << endl;
+    return;
+  }
+  
+  // solicitar el titulo de la cancion a buscar
+  cin.ignore();
+  cout << "Ingresa el titulo de la cancion a buscar:";
+  cin.getline(titBuscar, 50, '\n');
+  
+  // mostrar las canciones
+  cout << endl << "Buscando cancion con titulo: " << titBuscar << endl;
+  do{
+    if(strcmp(titBuscar, apCopia->titulo) == 0){
+      // mostrar  cancion
+      cout << "Cancion encontrada" << endl;
+      cout << endl << "============================" << endl;
+      cout << "Titulo: " << apCopia->titulo << endl;
+      cout << "Artista: " << apCopia->artista << endl;
+      cout << "Duracion: " << apCopia->duracion << endl;
+      cout << "Genero: " << apCopia->genero << endl;
+      return;
+    }
+    // ir a la cancion siguiente
+    apCopia = apCopia -> next;
+  } while(apPlayList != apCopia);
+  
+  cout << "Cancion no encontrada" << endl;
+  
+  return;
+} // buscar
 
 //! menu()
 //! ==============================================================
 void menu(void) {
   cout << endl << endl;
-  cout << "                           Playlist" << endl;
+  cout << "Playlist" << endl;
   cout << "====================================" << endl;
   cout << "1. Agregar cancion a la lista" << endl;
   cout << "2. Consultar canciones en la lista (Tarea)" << endl;
